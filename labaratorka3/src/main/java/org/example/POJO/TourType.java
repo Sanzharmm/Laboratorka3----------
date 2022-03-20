2da8591ec94b018620532b26bef3ae6540f8f8c8
Sanharmm <sanjarmelisov01@gmail.com>
2022-03-20 15:49:19 +0600

    Laboratorka3(ООП)

---
+++ labaratorka3/src/main/java/org/example/POJO/TourType.java
@@ -0,0 +1,13 @@
+package org.example.POJO;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tourtype")
+public class TourType {
+
+    @Id
+    @Column(name="")
+}
